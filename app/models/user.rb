40fcdc65eac7c2429e03ec7f39475de3b3666683
loed <loed20@gmail.com>
2012-12-07 10:54:40 +0100

    before create fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  before_save :edit_role
+  before_create :edit_role
 
   has_and_belongs_to_many :roles, :join_table => :users_roles
   has_many :sites
@@ -17,6 +17,11 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :roles, :name, :lastname, :phone, :country, :organisation, :comment, :street, :housenumber, :zip, :place, :btw, :kvk, :company_name, :notification_lead, :notification_sale, :notification_feed, :notification_result, :notification_status, :notification_merchant, :sites_attributes, :notice_attributes
   
+  # Validation rules
+  validates :email, :uniqueness => true
+  
+
+
   def role?(role) 
 
     if self.roles.find_by_name(role) 
