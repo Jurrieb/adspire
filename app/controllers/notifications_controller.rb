8faff7ffc8f795d1c20135697dc13a020d17262c
loed <loed20@gmail.com>
2012-12-13 11:33:57 +0100

    notifications working :D

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,11 +3,26 @@ class NotificationsController < ApplicationController
 	def settings
 		@notice = Notice.find_by_user_id(current_user.id)
 
+		if @notice.blank?
+			@notice = Notice.new
+			# Standard values
+			@notice.user_id = current_user.id
+			@notice.lead = true
+			@notice.sale = true
+			@notice.feed = false
+			@notice.result = true
+			@notice.status = false
+			@notice.merchant = false
+			@notice.action = true
+			@notice.save!
+		end
+
 	end
 
 	def settings_update
 		
 		@notice = Notice.find_by_user_id(current_user.id)
+		puts @notice.to_yaml
 
 		respond_to do |format|  
 	        if @notice.update_attributes(params[:notice])
