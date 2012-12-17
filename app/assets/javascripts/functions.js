553e386f902cba9f4e0b44a92226d0e309bbf90c
Sander Wind <sander@sanderwind.nl>
2012-12-17 14:10:05 +0100

    Form layouts

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('.dropdown-toggle').dropdown();
 	
+	// Notifications
 	$('#notifications li > button.remove').click(function() {
 		$(this).parent().slideUp('fast', function() { $(this).remove(); });
 
@@ -9,13 +10,29 @@ $(document).ready(function() {
 
 
 	// Form items
+	// Radio buttons
 	$('.btn-group[data-toggle="buttons-radio"]').each(function() {
+		var form_data = $(this).parents('form').attr('data');
 		var target = $(this).attr('data-target');
-		var value = $(this).find('button.active').val();
-		$('#'+target).val(value);
+		var value = $(this).parent().find('input[type="hidden"]').val() == 't' ? 1 : 0;
+
+		$('#'+form_data+'_'+target).val(value);
+		$(this).find('button[value="'+value+'"]').addClass('active '+(value?'btn-success':'btn-danger'));
 
 		$(this).find('button').click(function() {
-			$('#'+target).val($(this).val());
+			$('#'+form_data+'_'+target).val($(this).val());
+			$(this).parent().find('button').removeClass('btn-success btn-danger');
+			$(this).addClass($(this).val()==1?'btn-success':'btn-danger')
 		});
+	});
+	// Checkboxes
+	$('.btn-group[data-toggle="buttons-checkbox"]').each(function() {
+		var form_data = $(this).parents('form').attr('data');
+		var target = $(this).attr('data-target');
+		var value = $(this).find('button.active').val();
+		
+		if($(form_data+'['+target+']').length == 0) {
+			$(this).parent().prepend('<input type="hidden" name="'+form_data+'['+target+']" value=');
+		}
 	});
 });
