4068414e34713b8dd1a5d4c51c00fcb4d740fa36
Sander Wind <sander@sanderwind.nl>
2012-12-17 15:27:51 +0100

    Form layouts

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
 	$('.dropdown-toggle').dropdown();
 	
 	// Notifications
@@ -8,31 +8,41 @@ $(document).ready(function() {
 		return false;
 	});
 
-
 	// Form items
 	// Radio buttons
 	$('.btn-group[data-toggle="buttons-radio"]').each(function() {
 		var form_data = $(this).parents('form').attr('data');
 		var target = $(this).attr('data-target');
-		var value = $(this).parent().find('input[type="hidden"]').val() == 't' ? 1 : 0;
+		var value = $(this).parent().find('input[type="hidden"]').val();
+		var toggle = value == 't' || value == 'f';
+
+		if(toggle) {
+			value = value == 't' ? 1 : 0;
+		}
 
 		$('#'+form_data+'_'+target).val(value);
-		$(this).find('button[value="'+value+'"]').addClass('active '+(value?'btn-success':'btn-danger'));
+		
+		if(toggle) {
+			$(this).find('button[value="'+value+'"]').addClass('active '+(value?'btn-success':'btn-danger'));
+		}
 
 		$(this).find('button').click(function() {
 			$('#'+form_data+'_'+target).val($(this).val());
-			$(this).parent().find('button').removeClass('btn-success btn-danger');
-			$(this).addClass($(this).val()==1?'btn-success':'btn-danger')
+			
+			if(toggle) {
+				$(this).parent().find('button').removeClass('btn-success btn-danger');
+				$(this).addClass($(this).val()==1?'btn-success':'btn-danger')
+			}
 		});
 	});
-	// Checkboxes
-	$('.btn-group[data-toggle="buttons-checkbox"]').each(function() {
-		var form_data = $(this).parents('form').attr('data');
-		var target = $(this).attr('data-target');
-		var value = $(this).find('button.active').val();
-		
-		if($(form_data+'['+target+']').length == 0) {
-			$(this).parent().prepend('<input type="hidden" name="'+form_data+'['+target+']" value=');
-		}
-	});
+
 });
+
+// Feeds
+// Fields
+function toggleField(el) {
+	$(el).prev().find('select > option:first-child').toggle();
+	$(el).find('i').toggleClass('icon-plus icon-minus');
+	$(el).prev().toggle();
+	$(el).find('span').text($(el).find('i').hasClass('icon-plus')?'Use Field':'Remove Field');
+}
