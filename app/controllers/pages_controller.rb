267888b547fa7795c35fc71137ce250959b39608
loed <loed20@gmail.com>
2012-12-19 13:39:32 +0100

    removed comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,18 +17,6 @@ class PagesController < ApplicationController
 	# Group by day
 	@clicks_by_date = @clicks.group_by { |t| t.created_at.beginning_of_day }
 	@leads_by_date = @leads_from_clicks.group_by { |t| t.created_at.beginning_of_day }
-	
-	# All leads for current user
-	#
-
-	puts "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-	puts "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-	puts @leads_by_date
-	puts "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-	puts "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-
-	#@leads = Lead.where(:user_id => current_user.id).in_days.order('created_at ASC')
-	#@leads_by_date = @leads.group_by { |t| t.created_at.beginning_of_day }
 
 	# New array for graph
 	@graph = Array.new
@@ -43,16 +31,6 @@ class PagesController < ApplicationController
 		@graph << { y: day.strftime("%Y-%m-%d"), lead: counts.count }
 	end
 
-
-	# @leads.each do |l|
-	#	puts l.to_yaml
-	#	if @leads[l.created_at.to_i] == @graph[l.created_at.to_i]
-	 	   	
-	#	   	@leads_by_date.sort.each do |day, counts|
-	#	   		@graph << { y: day.strftime("%Y-%m-%d"), lead: counts.count }
-	#	   	end
-	#	end	
-	# end
 	# Respond to JSON
 	respond_to do |format|
 		format.json { render json: @graph }
