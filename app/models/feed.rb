bd12e8db3d05086a0c56ca6f0e84198b9b173d65
Jurgen Bosma <jurrieb@gmail.com>
2012-12-19 15:23:18 +0100

    NEW DELAYED JOB IT WORKS INSANELY

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -13,15 +13,81 @@ class Feed < ActiveRecord::Base
 	validates :name, :presence => true
 	validates :interval_in_seconds, :presence => true
 
-	validates_presence_of :url, :unless => :file?
-	validates_presence_of :file, :unless => :url?
 
 	RECORDS = Hash.new
 
 	def parse_feed
-		d = DelayedJob.first
-		d.queue = '2'
-		d.save
+		self.last_parse = Time.now
+		self.save
+
+		category_key =  Feednode.find(:first, :conditions => {:feed_id => self.id, :field_id => Field.find_by_name('category')})
+		producthash_key = Feednode.find(:first, :conditions => {:feed_id => self.id, :field_id => Field.find_by_name('unique hash')})
+
+		if producthash_key
+			if self.feed_path == nil
+				if !self.url.blank?
+					open_uri_fetched = open(self.url)
+					doc = Nokogiri::XML(open(open_uri_fetched))
+				else
+					self.status = 'NoFile'
+					self.save
+				end
+			else
+				f = File.open(self.feed_path)
+					doc = Nokogiri::XML(f)
+				f.close
+			end
+
+			if category_key 
+
+				foreign_categories = Array.new
+				doc.xpath(self.xml_path+'/'+category_key.name).each do |node|
+				    foreign_categories << node.text
+				end
+
+				if foreign_categories.count > 0
+					foreign_categories.uniq.each do |category|
+						if !ForeignCategory.find(:first, :conditions => {:feed_id => self.id, :name => category})	
+						  newkey = ForeignCategory.new
+						  newkey.feed_id = self.id
+						  newkey.name = category
+						  newkey.save
+						end
+					end
+				end
+
+			end	
+
+			products = Array.new
+			doc.xpath(self.xml_path).each do |product|
+			    records = Feednode.where('field_id <> ?', Field.where('name' => 'dont use').first)
+		       	productdata = Hash.new
+				records.each do |record|
+					if record.field.name == 'category'
+						productdata[:category_id] = ForeignCategory.find_by_name(product.xpath(record.name).text).id
+					else
+						productdata[record.field.product_column_name.to_sym] = product.xpath(record.name).text
+					end
+				end
+				productdata[:feed_id] = self.id
+				productdata[:user_id] = self.user_id
+				products << productdata
+			end
+
+
+			existing_product_hashes = Product.find(:all, :conditions => {:feed_id => self.id}, :select => "id").map{|p| p.id}
+			
+			products.each do |product|
+				existing_product = Product.find(:last, :conditions => {:feed_id => self.id, :unique_hash => product[:unique_hash]})
+				if existing_product
+					existing_product.update_attributes(product)
+					existing_product_hashes.delete(existing_product.id)
+				else
+					Product.create(product).save
+				end
+			end
+			Product.update_all({:status => 'inactive'}, {:id => existing_product_hashes})
+		end	
 	end
 
 	def map_feed
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,10 +1,11 @@
+require 'delayed_job_active_record'
 namespace :scheduler do
   desc "TODO"
   task :parse => :environment do
 	feed = Feed.find(:first, :conditions => {:status => 'active'})
-	feed.last_parse = Datetime.now
-	feed.save
-	feed.delay.parse_feed
+	if feed
+		feed.delay.parse_feed
+	end
   end
 
 end
